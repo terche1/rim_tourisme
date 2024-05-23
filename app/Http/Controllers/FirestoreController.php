65319ac41090345e4f8e5eb54de7dac6aec8932d
terche1 <tercha.abdou.15@icloud.com>
2024-05-23 01:39:45 +0000

    rim tourisme

---
+++ app/Http/Controllers/FirestoreController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Google\Cloud\Firestore\FirestoreClient;
+class FirestoreController extends Controller
+{
+  public function testFirestore()
+  {
+    // Initialize Firestore
+$firestore = new FirestoreClient([
+  'projectId' => 'pfe1-4c9de',
+]);
+
+// Test connection by retrieving data from Firestore
+$documents = $firestore->collection('admins')->documents();
+
+// Array to hold Firestore document data
+$documentDataArray = [];
+
+foreach ($documents as $document) {
+  // Add document data to the array
+  $documentDataArray[] = $document->data();
+}
+
+// Convert the array to a JSON string
+$jsonData = json_encode($documentDataArray);
+
+// Output the JSON data
+echo $jsonData;
+}
+}
